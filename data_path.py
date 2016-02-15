553742abdebe541e15c6d50b703588b9efcfe113
Xiaolu Xiong <beardeer@gmail.com>
2016-02-15 16:08:31 -0500

    change data path

--- data_path.py
+++ data_path.py
@@ -1,17 +1,18 @@
-from os import getcwd
+# from os import getcwd
+import os
 from getpass import getuser
 import platform
 
 USER_NAME = getuser()
 
 if platform.system() == 'Windows':
-	DROPBOX_FOLDER = "C:/Users/" + USER_NAME + "/Dropbox/"
+	DROPBOX_FOLDER = os.path.join("C:\\", 'Users', USER_NAME, 'Dropbox')
 else:
 	raise NotImplementedError()
 
-DROPBOX_DATA_FOLDER = DROPBOX_FOLDER + "Data/"
+DROPBOX_DATA_FOLDER = os.path.join(DROPBOX_FOLDER, 'Data')
 
-CURRENT_DEVELOPMENT_FOLDER = getcwd()
+CURRENT_DEVELOPMENT_FOLDER = os.getcwd()
 
 # CURRENT_DEVELOPMENT_DATA_FOLDER = CURRENT_DEVELOPMENT_FOLDER + "/Data/"
 
